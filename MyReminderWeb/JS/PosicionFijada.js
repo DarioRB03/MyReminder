7b6e3f4589a32c6f5b48b272d882f6f72730733d
XA13F1\1erDAM <bogoru@xuqueralumnat.es>
2022-06-01 11:41:32 +0200

    He reeditado la informacion de la pagina web

--- MyReminderWeb/JS/PosicionFijada.js
+++ MyReminderWeb/JS/PosicionFijada.js
@@ -13,23 +13,18 @@ $(window).scroll(function(){
     var diferencia = 1000 - y;
    /*console.log("top " + "max de 120 y " + diferencia);*/
 
-<<<<<<< HEAD
-  if(diferencia > 300){
-    $(".my-div-fixed").css("top",Math.max(120,800-$(this).scrollTop()));
-  } else if(diferencia < -230){
-    $(".my-div-fixed").css("top",Math.min(120,1600-$(this).scrollTop()));
-  }
-
-  
-=======
-  if(height > 2500){
+  if(height > 3000){
+    if(diferencia > 100){
+      $(".my-div-fixed").css("top",Math.max(120,1000-$(this).scrollTop()));
+    } else if(diferencia < 1000){
+      $(".my-div-fixed").css("top",Math.min(120,2100-$(this).scrollTop()));
+    }
+  } else if (height > 2500){
     if(diferencia > 300){
       $(".my-div-fixed").css("top",Math.max(120,800-$(this).scrollTop()));
     } else if(diferencia < -230){
       $(".my-div-fixed").css("top",Math.min(120,1600-$(this).scrollTop()));
     }
-  }
-
->>>>>>> 4a670b3d70d8ffb65dfda74bd77a0a132c720360
+  } 
 
 })
