d157258b812494fa446aad950145d44501fd1a0a
XA13F1\1erDAM <bogoru@xuqueralumnat.es>
2022-06-01 11:30:48 +0200

    He añadido la informacion de la apliaciona a la web

--- MyReminderWeb/JS/PosicionFijada.js
+++ MyReminderWeb/JS/PosicionFijada.js
@@ -13,15 +13,12 @@ $(window).scroll(function(){
     /*var diferencia = 1000 - y;
     console.log("top " + "max de 120 y " + diferencia);*/
 
-if(height > 4174){
   if(diferencia > 300){
     $(".my-div-fixed").css("top",Math.max(120,800-$(this).scrollTop()));
   } else if(diferencia < -230){
     $(".my-div-fixed").css("top",Math.min(120,1600-$(this).scrollTop()));
   }
-} else if(0 < 1){
 
-}
   
 
 })
