a6b14a3fa6f4231bf767518357ac027832386ee7
Alex Santos <alexblasco18@gmail.com>
2022-06-03 02:50:56 +0200

    Estructura de los HTML de Video finalizada.

---
+++ MyReminderWeb/JS/Video.js
@@ -0,0 +1,13 @@
+var videoPlayer = document.getElementById("videoPlayer");
+var myVideo = document.getElementById("myVideo");
+
+function stopVideo(){
+
+  videoPlayer.style.display = "none";
+
+}
+
+function playVideo(file){
+  myVideo.src = file;
+  videoPlayer.style.display = "block";
+}
